d6028e828f665531bc53cef9153b88fa594540f9
StefaniAlmeida <almeidastefani099@gmail.com>
2017-09-01 20:44:03 -0300

    Update Agenda.py

--- AgendaContatoApp/Model/Agenda.py
+++ AgendaContatoApp/Model/Agenda.py
@@ -1,2 +1,13 @@
 class Agenda():
-    pass
+    def __init__ (self, proprietario, contatos=[]):
+        self.proprietario = proprietario
+        self.contatos = contatos
+
+    def contarContatos(self):
+       return len(self.contatos)
+
+    def listarContatos(self):
+        return self.contatos
+        
+    def incluirContato(self, contato):
+        self.contatos.append(contato)
