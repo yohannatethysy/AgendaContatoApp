15e539905a7f0f12a02658dcf84c164154757650
StefaniAlmeida <almeidastefani099@gmail.com>
2017-09-01 20:48:28 -0300

    Update Contato.py

--- AgendaContatoApp/Model/Contato.py
+++ AgendaContatoApp/Model/Contato.py
@@ -1,2 +1,10 @@
+cimport datetime
+
 class Contato():
-    pass
+    def __init__(self, pessoa, telefone):
+        self.criacao = str(datetime.date.today())
+        self.pessoa = pessoa
+        self.telefone = telefone
+
+    def listarTelefones(self):
+        pass
