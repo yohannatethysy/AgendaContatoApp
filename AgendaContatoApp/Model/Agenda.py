3fbb3110084aa7916fefafacf91a99f4aad178d7
yohannatethysy <thetysy@gmail.com>
2017-09-01 23:23:11 -0300

    Update Agenda.py

--- AgendaContatoApp/Model/Agenda.py
+++ AgendaContatoApp/Model/Agenda.py
@@ -11,3 +11,27 @@ class Agenda():
         
     def incluirContato(self, contato):
         self.contatos.append(contato)
+
+    def buscarContato(self, nome):
+        nome = nome.lower()
+        encontrado = False
+        for i in self.contatos:
+            if i['pessoa']['nome'] == nome:
+                encontrado = True
+                return(i)
+        if encontrado == False:
+            return "Nome não encontrado."
+
+    def excluirContato(self, nome):
+        nome = nome.lower()
+        encontrado = False
+        for i in self.contatos:
+            if i['pessoa]']['nome'] == nome:
+                encontrado = True
+                self.contatos.remove(nome)
+        if encontrado == False:
+            return "Nome não encontrado."
+
+    def salvar(self, contato):
+        cont = open('Agenda.json', 'w')
+        cont.write(contato)        
