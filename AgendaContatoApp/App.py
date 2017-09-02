635e31499055fc1178061c31fb45e974d1470fbe
yohannatethysy <thetysy@gmail.com>
2017-09-01 23:21:25 -0300

    Update App.py

--- AgendaContatoApp/App.py
+++ AgendaContatoApp/App.py
@@ -45,4 +45,83 @@ def main():
         v = open("Agenda.json", "w", encoding='utf8')
 
         v.write(aJson)
-       
+
+    def listarContatos():
+        v = open("Agenda.json", "r", encoding='utf8')
+        agendaJson = json.load(v)
+        agenda = Agenda(agendaJson['proprietario'], agendaJson['contatos'])
+
+        for contato in agenda.listarContatos():
+            print(contato)
+
+    def excluirContatos():
+        v = open("Agenda.json", "r", encoding='utf8')
+        agendaJson = json.load(v)
+        agenda = Agenda(agendaJson['proprietario'], agendaJson['contatos'])
+
+        nome = input("Qual o contato que deve ser deletado? ")
+        agenda.excluirContato(nome)
+
+        aJson = json.dumps(para_dict(agenda))
+
+        v = open("Agenda.json", "w", encoding='utf8')
+
+        v.write(aJson)
+
+    def buscarContato():
+        v = open("Agenda.json", "r", encoding='utf8')
+        agendaJson = json.load(v)
+        agenda = Agenda(agendaJson['proprietario'], agendaJson['contatos'])
+
+        nome = input("Qual o nome do contato que você deseja buscar? ")
+        agenda.buscarContato(nome)
+
+    def contarContatos():
+        pass
+
+
+    try:
+        v = open("Agenda.json", "r", encoding='utf8')
+        print("Abrimos a agenda porque a encontramos :)")
+    except FileNotFoundError:
+        v = open("Agenda.json", "w", encoding='utf8')
+        nome = input("Qual o nome do contato a ser adicionado? ")
+        nascimento = input("Qual o nascimento do contato? ")
+        email = input("Qual o email do contato? ")
+        pessoa = Pessoa(nome, nascimento, email)
+        a2 = Agenda(pessoa)
+        a2Json = json.dumps(para_dict(a2))
+        v.write(a2Json)
+        print("Agenda criada.")
+
+    print("Menu:\n "
+            "1- Incluir contato\n"
+            "2- Listar contatos\n"
+            "3- Excluir contato\n"
+            "4- Buscar contato\n"
+            "5- Contar contatos\n"
+            "6- Sair")
+
+    try:
+        op = input("Digite a opção desejada: ")
+
+        if op == 1:
+            incluirNovoContato()
+        elif op == 2:
+            listarContatos()
+        elif op == 3:
+            excluirContatos()
+        elif op == 4:
+            buscarContato()
+        elif op == 5:
+            contarContatos()
+        elif op == 6:
+            break
+
+    except ValueError:
+        print("Só números são válidos... Tente novamente!")
+
+
+if __name__ == '__main__':
+    main()
+      
